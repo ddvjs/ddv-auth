3e23953a7c7deb6dd6fd01a8cdb8315893d36195
hadiChan <861883474@qq.com>
2017-07-17 16:27:55 +0800

    fixbug

--- util/index.js
+++ util/index.js
@@ -162,6 +162,107 @@ Object.assign(util, {
 Object.assign(util, {
   nextTick: require('./nextTick.js')
 })
+// 类型
+var class2type = (function () {
+  var t = {}
+  // Populate the class2type map
+  'Boolean Number String Function Array Date RegExp Object Error'.split(' ').forEach(function (name) {
+    t[ '[object ' + name + ']' ] = name.toLowerCase()
+  })
+  return t
+}())
+
+Object.assign(util, {
+  type: function type (obj, isType) {
+    if (isType !== void 0) {
+      return isType === util.type(obj)
+    }
+    if (obj === void 0) {
+      return obj + ''
+    }
+    // Support: Android<4.0, iOS<6 (functionish RegExp)
+    return (typeof obj === 'object' || typeof obj === 'function') ? class2type[ class2type.toString.call(obj) ] || 'object' : typeof obj
+  },
+  isPlainObject: function isPlainObject (obj) {
+    // Not plain objects:
+    // - Any object or value whose internal [[Class]] property is not "[object Object]"
+    // - DOM nodes
+    // - window
+    if (util.type(obj) !== 'object' || obj.nodeType || util.isGlobal(obj)) {
+      return false
+    }
+
+    if (obj.constructor && !Object.hasOwnProperty.call(obj.constructor.prototype, 'isPrototypeOf')) {
+      return false
+    }
+
+    // If the function hasn't returned already, we're confident that
+    // |obj| is a plain object, created by {} or constructed with new Object
+    return true
+  },
+  extend: function extend () {
+    var options, name, src, copy, copyIsArray, clone
+    var target = arguments[ 0 ] || {}
+    var i = 1
+    var length = arguments.length
+    var deep = false
+    // Handle a deep copy situation
+    if (typeof target === 'boolean') {
+      deep = target
+
+    // Skip the boolean and the target
+      target = arguments[ i ] || {}
+      i++
+    }
+
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== 'object' && !util.isFunction(target)) {
+      target = {}
+    }
+
+    // Extend jQuery itself if only one argument is passed
+    if (i === length) {
+      target = this
+      i--
+    }
+
+    for (; i < length; i++) {
+      // Only deal with non-null/undefined values
+      if ((options = arguments[i]) !== void 0) {
+        // Extend the base object
+        for (name in options) {
+          src = target[ name ]
+          copy = options[ name ]
+
+          // Prevent never-ending loop
+          if (target === copy) {
+            continue
+          }
+
+          // Recurse if we're merging plain objects or arrays
+          if (deep && copy && (util.isPlainObject(copy) || (copyIsArray = util.isArray(copy)))) {
+            if (copyIsArray) {
+              copyIsArray = false
+              clone = src && util.isArray(src) ? src : []
+            } else {
+              clone = src && util.isPlainObject(src) ? src : Object.create(null)
+            }
+
+            // Never move original objects, clone them
+            target[name] = util.extend(deep, clone, copy)
+
+          // Don't bring in undefined values
+          } else if (copy !== undefined) {
+            target[name] = copy
+          }
+        }
+      }
+    }
+
+    // Return the modified object
+    return target
+  }
+})
 
 function util () {
 
--- util/url.js
+++ util/url.js
@@ -4,9 +4,6 @@ var url = require('./url.base.fn.js')
 // 工具
 module.exports = url
 Object.assign(url, {
-  build: function build (obj) {
-
-  },
   parse: function parse (uri) {
     var r = {
       scheme: 'http',
@@ -125,4 +122,160 @@ Object.assign(url, {
       return key
     }
   }
+})
+Object.assign(url, {
+  /**
+	 * Build a URL.
+	 *
+	 * The parts of the second URL will be merged into the first according to
+	 * the flags argument.
+	 *
+	 * @param mixed urli     (part(s) of) an URL in form of a string or
+	 *                       associative array like parse_url() returns
+	 * @param mixed parts   same as the first argument
+	 * @param int   flags   a bitmask of binary or'ed HTTP_URL constants;
+	 *                       HTTP_URL_REPLACE is the default
+	 * @param array new_url if set, it will be filled with the parts of the
+	 *                       composed url like parse_url() would return
+	 * @return string
+	 */
+	build: function build(urli, parts, flags)
+	{
+    urli = urli ||  {}
+    parts = parts || {}
+    flags = flags || url.HTTP_URL_REPLACE
+		typeof(url) === 'object' || urli = url.parse(url);
+		typeof(parts) === 'object' || parts = url.parse(parts);
+
+		// isset(urli['query']) && is_string(urli['query']) || urli['query'] = null;
+		// isset(parts['query']) && is_string(parts['query']) || parts['query'] = null;
+
+    var keys = ['user', 'pass', 'port', 'path', 'query', 'fragment']
+		//$keys = array('user', 'pass', 'port', 'path', 'query', 'fragment');
+
+		// HTTP_URL_STRIP_ALL and HTTP_URL_STRIP_AUTH cover several other flags.
+		if (flags & url.HTTP_URL_STRIP_ALL) {
+			flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS
+				| url.HTTP_URL_STRIP_PORT | url.HTTP_URL_STRIP_PATH
+				| url.HTTP_URL_STRIP_QUERY | url.HTTP_URL_STRIP_FRAGMENT;
+		} else if (flags & url.HTTP_URL_STRIP_AUTH) {
+			flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS;
+		}
+
+		// Schema and host are alwasy replaced
+		var t = ['scheme', 'host']
+    var i
+    for (i = 0; i < t.length; i++) {
+			if (parts&&t[i]&&parts[t[i]]) {
+				urli[t[i]] = parts[t[i]];
+			}
+    }
+
+		if (flags & url.HTTP_URL_REPLACE) {
+      for (i = 0; i < keys.length; i++) {
+  			if (parts&&keys[i]&&parts[keys[i]]) {
+  				urli[keys[i]] = parts[keys[i]];
+  			}
+      }
+		} else {
+			if (parts&&parts['path'] && (flags & url.HTTP_URL_JOIN_PATH)) {
+				if (urli&&urli['path'] && (parts['path']).substr(0, 1) !== '/') {
+          var leftTemp, rigthTemp
+					// Workaround for trailing slashes
+					leftTemp = rigthTemp = ''
+					if (parts['path']) {
+            parts['path'] = (parts['path'] || '').toString()
+					  i = parts['path'].indexOf('/')
+            if (i>-1) {
+              rigthTemp = parts['path'].substr(i+1)
+            }
+					}
+          if (urli['path']) {
+            urli['path'] = (urli['path'] || '').toString()
+            i = urli['path'].lastIndexOf('/')
+            if (i>-1) {
+              leftTemp = urli['path'].substr(0,i)
+            }
+          }
+          urli['path'] = rigthTemp?leftTemp:(leftTemp+'/'+rigthTemp)
+				} else {
+					urli['path'] = parts['path'];
+				}
+			}
+
+			if (parts && parts['query'] && (flags & url.HTTP_URL_JOIN_QUERY)) {
+				if (isset(urli['query'])) {
+					var urliQuery = url.parseQuery(urli['query']);
+					var partsQuery = url.parseQuery(parts['query']);
+					urli['query'] = url.buildQuery(util.extend({},urliQuery,urliQuery));
+				} else {
+					urli['query'] = parts['query'];
+				}
+			}
+		}
+
+		if (isset(urli['path']) && urli['path'] !== '' && substr(urli['path'], 0, 1) !== '/') {
+			urli['path'] = '/' . urli['path'];
+		}
+
+    var strip
+    for (i = 0; i < keys.length; i++) {
+  		strip = 'HTTP_URL_STRIP_' + (keys[i]||'').toUpperCase();
+			if (flags & url[strip]) {
+				delete urli[keys[i]||''];
+			}
+    }
+
+		parsed_string = '';
+
+		if (urli['scheme']) {
+			parsed_string += urli['scheme'] + '://';
+		}
+
+		if (urli['user']) {
+			parsed_string += urli['user'];
+
+			if (urli['pass']) {
+				parsed_string += ':' + urli['pass'];
+			}
+
+			parsed_string += '@';
+		}
+
+		if (urli['host']) {
+			parsed_string += urli['host'];
+		}
+
+		if (urli['port']) {
+			parsed_string += ':' + urli['port'];
+		}
+
+		if (urli['path']) {
+			parsed_string += urli['path'];
+		}
+
+		if (urli['query']) {
+			parsed_string += '?' + urli['query'];
+		}
+
+		if (urli['fragment']) {
+			parsed_string += '#' + urli['fragment'];
+		}
+
+		return parsed_string;
+	}
+})
+
+Object.assign(url, {
+  'HTTP_URL_REPLACE': 1,
+  'HTTP_URL_JOIN_PATH': 2,
+  'HTTP_URL_JOIN_QUERY': 4,
+  'HTTP_URL_STRIP_USER': 8,
+  'HTTP_URL_STRIP_PASS': 16,
+  'HTTP_URL_STRIP_AUTH': 32,
+  'HTTP_URL_STRIP_PORT': 64,
+  'HTTP_URL_STRIP_PATH': 128,
+  'HTTP_URL_STRIP_QUERY': 256,
+  'HTTP_URL_STRIP_FRAGMENT': 512,
+  'HTTP_URL_STRIP_ALL': 1024
 })
