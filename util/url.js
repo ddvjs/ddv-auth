e7b31c8402bdfe001516cfcfbef8a1e9ab5e6e13
hua <yuchonghua@163.com>
2017-07-17 16:31:31 +0800

    fixbug

--- util/url.js
+++ util/url.js
@@ -125,145 +125,144 @@ Object.assign(url, {
 })
 Object.assign(url, {
   /**
-	 * Build a URL.
-	 *
-	 * The parts of the second URL will be merged into the first according to
-	 * the flags argument.
-	 *
-	 * @param mixed urli     (part(s) of) an URL in form of a string or
-	 *                       associative array like parse_url() returns
-	 * @param mixed parts   same as the first argument
-	 * @param int   flags   a bitmask of binary or'ed HTTP_URL constants;
-	 *                       HTTP_URL_REPLACE is the default
-	 * @param array new_url if set, it will be filled with the parts of the
-	 *                       composed url like parse_url() would return
-	 * @return string
-	 */
-	build: function build(urli, parts, flags)
-	{
-    urli = urli ||  {}
+   * Build a URL.
+   *
+   * The parts of the second URL will be merged into the first according to
+   * the flags argument.
+   *
+   * @param mixed urli     (part(s) of) an URL in form of a string or
+   *                       associative array like parse_url() returns
+   * @param mixed parts   same as the first argument
+   * @param int   flags   a bitmask of binary or'ed HTTP_URL constants;
+   *                       HTTP_URL_REPLACE is the default
+   * @param array new_url if set, it will be filled with the parts of the
+   *                       composed url like parse_url() would return
+   * @return string
+   */
+  build: function build (urli, parts, flags) {
+    urli = urli || {}
     parts = parts || {}
     flags = flags || url.HTTP_URL_REPLACE
-		typeof(url) === 'object' || urli = url.parse(url);
-		typeof(parts) === 'object' || parts = url.parse(parts);
+    typeof url === 'object' || (urli = url.parse(url))
+    typeof parts === 'object' || (parts = url.parse(parts))
 
-		// isset(urli['query']) && is_string(urli['query']) || urli['query'] = null;
-		// isset(parts['query']) && is_string(parts['query']) || parts['query'] = null;
+    // isset(urli['query']) && is_string(urli['query']) || urli['query'] = null;
+    // isset(parts['query']) && is_string(parts['query']) || parts['query'] = null;
 
     var keys = ['user', 'pass', 'port', 'path', 'query', 'fragment']
-		//$keys = array('user', 'pass', 'port', 'path', 'query', 'fragment');
+    // $keys = array('user', 'pass', 'port', 'path', 'query', 'fragment');
 
-		// HTTP_URL_STRIP_ALL and HTTP_URL_STRIP_AUTH cover several other flags.
-		if (flags & url.HTTP_URL_STRIP_ALL) {
-			flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS
-				| url.HTTP_URL_STRIP_PORT | url.HTTP_URL_STRIP_PATH
-				| url.HTTP_URL_STRIP_QUERY | url.HTTP_URL_STRIP_FRAGMENT;
-		} else if (flags & url.HTTP_URL_STRIP_AUTH) {
-			flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS;
-		}
-
-		// Schema and host are alwasy replaced
-		var t = ['scheme', 'host']
-    var i
-    for (i = 0; i < t.length; i++) {
-			if (parts&&t[i]&&parts[t[i]]) {
-				urli[t[i]] = parts[t[i]];
-			}
+    // HTTP_URL_STRIP_ALL and HTTP_URL_STRIP_AUTH cover several other flags.
+    if (flags & url.HTTP_URL_STRIP_ALL) {
+      flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS |
+        url.HTTP_URL_STRIP_PORT | url.HTTP_URL_STRIP_PATH |
+        url.HTTP_URL_STRIP_QUERY | url.HTTP_URL_STRIP_FRAGMENT
+    } else if (flags & url.HTTP_URL_STRIP_AUTH) {
+      flags |= url.HTTP_URL_STRIP_USER | url.HTTP_URL_STRIP_PASS
     }
 
-		if (flags & url.HTTP_URL_REPLACE) {
-      for (i = 0; i < keys.length; i++) {
-  			if (parts&&keys[i]&&parts[keys[i]]) {
-  				urli[keys[i]] = parts[keys[i]];
-  			}
+    // Schema and host are alwasy replaced
+    var t = ['scheme', 'host']
+    var i
+    for (i = 0; i < t.length; i++) {
+      if (parts && t[i] && parts[t[i]]) {
+        urli[t[i]] = parts[t[i]]
       }
-		} else {
-			if (parts&&parts['path'] && (flags & url.HTTP_URL_JOIN_PATH)) {
-				if (urli&&urli['path'] && (parts['path']).substr(0, 1) !== '/') {
+    }
+
+    if (flags & url.HTTP_URL_REPLACE) {
+      for (i = 0; i < keys.length; i++) {
+        if (parts && keys[i] && parts[keys[i]]) {
+          urli[keys[i]] = parts[keys[i]]
+        }
+      }
+    } else {
+      if (parts && parts['path'] && (flags & url.HTTP_URL_JOIN_PATH)) {
+        if (urli && urli['path'] && (parts['path']).substr(0, 1) !== '/') {
           var leftTemp, rigthTemp
-					// Workaround for trailing slashes
-					leftTemp = rigthTemp = ''
-					if (parts['path']) {
+          // Workaround for trailing slashes
+          leftTemp = rigthTemp = ''
+          if (parts['path']) {
             parts['path'] = (parts['path'] || '').toString()
-					  i = parts['path'].indexOf('/')
-            if (i>-1) {
-              rigthTemp = parts['path'].substr(i+1)
+            i = parts['path'].indexOf('/')
+            if (i > -1) {
+              rigthTemp = parts['path'].substr(i + 1)
             }
-					}
+          }
           if (urli['path']) {
             urli['path'] = (urli['path'] || '').toString()
             i = urli['path'].lastIndexOf('/')
-            if (i>-1) {
-              leftTemp = urli['path'].substr(0,i)
+            if (i > -1) {
+              leftTemp = urli['path'].substr(0, i)
             }
           }
-          urli['path'] = rigthTemp?leftTemp:(leftTemp+'/'+rigthTemp)
-				} else {
-					urli['path'] = parts['path'];
-				}
-			}
+          urli['path'] = rigthTemp ? leftTemp : (leftTemp + '/' + rigthTemp)
+        } else {
+          urli['path'] = parts['path']
+        }
+      }
 
-			if (parts && parts['query'] && (flags & url.HTTP_URL_JOIN_QUERY)) {
-				if (isset(urli['query'])) {
-					var urliQuery = url.parseQuery(urli['query']);
-					var partsQuery = url.parseQuery(parts['query']);
-					urli['query'] = url.buildQuery(util.extend({},urliQuery,urliQuery));
-				} else {
-					urli['query'] = parts['query'];
-				}
-			}
-		}
+      if (parts && parts['query'] && (flags & url.HTTP_URL_JOIN_QUERY)) {
+        if (urli && urli['query']) {
+          var urliQuery = url.parseQuery(urli['query'])
+          var partsQuery = url.parseQuery(parts['query'])
+          urli['query'] = url.buildQuery(util.extend({}, urliQuery, partsQuery))
+        } else {
+          urli['query'] = parts['query']
+        }
+      }
+    }
 
-		if (isset(urli['path']) && urli['path'] !== '' && substr(urli['path'], 0, 1) !== '/') {
-			urli['path'] = '/' . urli['path'];
-		}
+    if (urli && urli['path'] && urli['path'] !== '' && (urli['path'] || '').substr(0, 1) !== '/') {
+      urli['path'] = '/'.urli['path']
+    }
 
     var strip
     for (i = 0; i < keys.length; i++) {
-  		strip = 'HTTP_URL_STRIP_' + (keys[i]||'').toUpperCase();
-			if (flags & url[strip]) {
-				delete urli[keys[i]||''];
-			}
+      strip = 'HTTP_URL_STRIP_' + (keys[i] || '').toUpperCase()
+      if (flags & url[strip]) {
+        delete urli[keys[i] || '']
+      }
     }
 
-		parsed_string = '';
+    var parsedString = ''
 
-		if (urli['scheme']) {
-			parsed_string += urli['scheme'] + '://';
-		}
+    if (urli['scheme']) {
+      parsedString += urli['scheme'] + '://'
+    }
 
-		if (urli['user']) {
-			parsed_string += urli['user'];
+    if (urli['user']) {
+      parsedString += urli['user']
 
-			if (urli['pass']) {
-				parsed_string += ':' + urli['pass'];
-			}
+      if (urli['pass']) {
+        parsedString += ':' + urli['pass']
+      }
 
-			parsed_string += '@';
-		}
+      parsedString += '@'
+    }
 
-		if (urli['host']) {
-			parsed_string += urli['host'];
-		}
+    if (urli['host']) {
+      parsedString += urli['host']
+    }
 
-		if (urli['port']) {
-			parsed_string += ':' + urli['port'];
-		}
+    if (urli['port']) {
+      parsedString += ':' + urli['port']
+    }
 
-		if (urli['path']) {
-			parsed_string += urli['path'];
-		}
+    if (urli['path']) {
+      parsedString += urli['path']
+    }
 
-		if (urli['query']) {
-			parsed_string += '?' + urli['query'];
-		}
+    if (urli['query']) {
+      parsedString += '?' + urli['query']
+    }
 
-		if (urli['fragment']) {
-			parsed_string += '#' + urli['fragment'];
-		}
+    if (urli['fragment']) {
+      parsedString += '#' + urli['fragment']
+    }
 
-		return parsed_string;
-	}
+    return parsedString
+  }
 })
 
 Object.assign(url, {
